a7746026c9f4e39e668a2a59e8b31b0d12e678ed
Himanshu Singh <shimanshusingh9532@gmail.com>
2024-05-04 21:40:57 +0530

    .

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -5,7 +5,7 @@ import Footer from "../Footer";
 import HeroSection from "../HeroSection";
 import MyPortfolio from "../MyPortfolio";
 import MySkills from "../MySkills";
-import Testimonial from "../Testimonials";
+
 
 export default function Home() {
   return (
